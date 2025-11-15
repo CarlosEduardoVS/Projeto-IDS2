d01eb35fdf87253f041adc760c47ec8cdcd0f863
Carlos Eduardo <105132190+CarlosEduardoVS@users.noreply.github.com>
2025-11-14 23:19:31 -0300

    Projeto utilizado na materia anterior
    esse Projeto foi utilizado na matéria anterior e será atualizado para os requisitos desse trabalho

---
+++ Projeto-IDS2/Projeto-IDS2/src/main/java/com/Projeto_IDS2/Projeto_IDS2/ProjetoIds2Application.java
@@ -0,0 +1,13 @@
+package com.Projeto_IDS2.Projeto_IDS2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIds2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIds2Application.class, args);
+	}
+
+}
